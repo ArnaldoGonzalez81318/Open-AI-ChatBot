71921ebb709d20bcd87a7c5cf5484ffc7e2000a4
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2025-11-20 22:51:42 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,1 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+export { default } from './tailwind.config.ts';
