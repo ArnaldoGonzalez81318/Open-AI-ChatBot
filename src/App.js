ec883d20b236e172fbce434a4ddcf85c3e93d1b1
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2025-11-20 22:51:07 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,1 @@
-import React from 'react';
-import Main from './Pages/Main';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header"></header>
-      <Main />
-    </div>
-  );
-}
-
-export default App;
+export { default } from './App.tsx';
