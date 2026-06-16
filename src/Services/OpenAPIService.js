cee70be173f4f733d2ecdeaef757e36313a4bcf5
Arnaldo Gonzalez <arnaldolgonzalez96@gmail.com>
2026-06-16 11:29:44 -0400

    Update OpenAPIService.js

--- src/Services/OpenAPIService.js
+++ src/Services/OpenAPIService.js
@@ -1,3 +1,3 @@
 throw new Error(
-  'OpenAPIService.js has been superseded by the TypeScript hook in src/hooks/useChatApi.ts. Update your imports accordingly.'
+  "OpenAPIService.js has been superseded by the TypeScript hook in src/hooks/useChatApi.ts. Update your imports accordingly.",
 );
